b757cd6582f8448bf59696a57a35e32ad9426ced
Abis <abisali@hotmail.com>
2022-09-28 13:50:07 +0400

    route config

---
+++ api_server/routes/testapi.js
@@ -0,0 +1,10 @@
+console.log(1);
+
+var express = require("express");
+var router = express.Router();
+
+router.get("/", function(req, res, next) {
+    res.send("API is working properly");
+});
+
+module.exports = router;
