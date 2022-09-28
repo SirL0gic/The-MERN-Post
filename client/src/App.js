db45c3392f276c86572e5a1b404ecf4171abceff
Abis <abisali@hotmail.com>
2022-09-28 15:12:12 +0400

    configs

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
-
-
+import React, { Component, useState, useEffect } from "react";
 
 function App() {
 
+  const [backendData, setBackendData] = useState([{}])
+  useEffect(() => {
+    fetch("/api").then(
+      response => response.json()
+    ).then(
+      data => {
+        setBackendData(data)
+      }
+    )
+  }, [])
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Hello CodeSandbox</h1>
+      <h2>Start editing to see some magic happen!</h2>
+      <h3>{backendData.users}</h3>
+
+      
     </div>
   );
 }
 
-export default App;
+export default App
