3876ffd7f938fcfdb98a13c9257d07b03ba50580
Abis <abisali@hotmail.com>
2022-09-29 13:06:44 +0400

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -17,7 +17,6 @@ function App() {
   return (
     <div>
       <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
       <h3>{backendData.users}</h3>
     </div>
   );
