043003822cd1d5aa9c6c746ff974776ded4358fe
Abis <abisali@hotmail.com>
2022-10-04 16:04:13 +0400

    y

--- client/src/components/weather.js
+++ client/src/components/weather.js
@@ -1,9 +1,20 @@
 import ReactWeather from "react-open-weather-widget";
 import "react-open-weather-widget/lib/css/ReactWeather.css";
+import '../css/weather.css'
+
+
+//remove a certain class
+// const boxes = document.querySelectorAll('.rw-box-right');
+
+// boxes.forEach(box => {
+//   box.remove();
+// });
+
 
 export default function WeatherWidget() {
     return (
-      <div className="Widget">
+        
+      <div className="w-widget">
         <ReactWeather
           forecast="today"
           apikey="f6973195b3a29969a6ad7e393d2ac38b"
