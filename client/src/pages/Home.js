ab8ffa68e58fbc9b9469d1baf025771e441fe06e
Abis <abisali@hotmail.com>
2022-10-04 12:55:33 +0400

    news

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,21 +1,27 @@
-import DateTime from '../components/Date';
-
+//Importing Hooks
 import React, {useState, useEffect } from "react";
-//extra css fro BS
+
+//Extra css for BS
 import 'bootstrap/dist/css/bootstrap.min.css';
+
 // Bootstrap CSS
 import "bootstrap/dist/css/bootstrap.min.css";
+
 // Bootstrap Bundle JS
 import "bootstrap/dist/js/bootstrap.bundle.min";
+
 // Bootstrap npm lib
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+
 //Custom css
 import '../css/custom.css'
-import NavBar from '../components/Nav';
 
+//Importing Components
+import NavBar from '../components/Nav';
 import Header from '../components/Head';
+import DateTime from '../components/Date';
 
 
 let HomePage = () => {
@@ -23,7 +29,7 @@ let HomePage = () => {
     return(
         <Container fluid>
             <Row>
-                <Col  xxl={12} className='no'>
+                <Col  xxl={12} className='no-col-nav'>
                 <div className="text-center">
                     <NavBar />
                 </div>
@@ -42,9 +48,15 @@ let HomePage = () => {
                 </div>
                 </Col>
             </Row>
-            
+
             <Row>
-                <Col>3 of 3</Col>
+                <Col>
+                <div className='news-area'>
+                    <div className="text-center">
+
+                    </div>
+                </div>
+                </Col>
             </Row>
         </Container>
     );
