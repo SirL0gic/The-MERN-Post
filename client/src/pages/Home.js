fcf4c593408f6e5ce9fc4a8cfb71dbcc1c7a33ab
Abis <abisali@hotmail.com>
2022-10-05 00:49:15 +0400

    Update Home.js

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -5,87 +5,77 @@
 import "bootstrap/dist/js/bootstrap.bundle.min";
 
 // Bootstrap npm lib
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-
-
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
 //Importing Components
-import NavBar from '../components/Nav';
-import Header from '../components/Head';
-import DateTime from '../components/Date';
-import NewsCard from '../components/News'
+import NavBar from "../components/Nav";
+import Header from "../components/Head";
+import DateTime from "../components/Date";
+import NewsCard from "../components/News";
 import WeatherWidget from "../components/weather";
-import { Timeline } from 'react-twitter-widgets';
+import { Timeline } from "react-twitter-widgets";
 
 // Bootstrap CSS
 import "bootstrap/dist/css/bootstrap.min.css";
 
-
 //Custom css
-import '../css/custom.css'
-
+import "../css/custom.css";
 
 let HomePage = () => {
-    
-    return(
-        <Container fluid>
-            <Row>
-                <Col  xxl={12} className='no-col-nav'>
-                <div className="text-center">
-                    <NavBar />
-                </div>
-                </Col>
-            </Row>
+  return (
+    <Container fluid>
+      <Row>
+        <Col xxl={12} className="no-col-nav">
+          <div className="text-center">
+            <NavBar />
+          </div>
+        </Col>
+      </Row>
 
-            <Row>
-                <Col xxl={12}>
-                <div className="main-heading-area">
-                    <h1 className="text-center">{<Header />}</h1>
-                </div>
-                <div className="date-area">
-                    <div className="text-center"> 
-                    <p className="date-display">{<DateTime />}</p>
-                    </div>
-                </div>
-                </Col>
-            </Row>
+      <Row>
+        <Col xxl={12}>
+          <div className="main-heading-area">
+            <h1 className="text-center">{<Header />}</h1>
+          </div>
+          <div className="date-area">
+            <div className="text-center">
+              <p className="date-display">{<DateTime />}</p>
+            </div>
+          </div>
+        </Col>
+      </Row>
 
-            <Row>
-                <Col lg={8}>
-                    <div className="news-area">
-                        <NewsCard />
-                        <br></br>
-                        <br></br>
-                        <NewsCard />
-                        <br></br>
-                        <br></br>
-                        <NewsCard />
-                    </div>
-                </Col>
+      <Row>
+        <Col lg={8}>
+          <div className="news-area">
+            <NewsCard />
+            <br></br>
+            <br></br>
+            <NewsCard />
+            <br></br>
+            <br></br>
+            <NewsCard />
+          </div>
+        </Col>
 
-                <Col lg={4}>
-                <WeatherWidget />
-                
-                <Timeline
-                    dataSource={{
-                        sourceType: 'profile',
-                        screenName: 'TwitterDev'
-                    }}
-                    options={{
-                        height: '400'
-                    }}
-                    />
-                </Col>
-            </Row>
+        <Col lg={4}>
+          <WeatherWidget />
 
-        </Container>
-    );
-}
+          <Timeline
+            dataSource={{
+              sourceType: "profile",
+              screenName: "TwitterDev",
+            }}
+            options={{
+              height: "400",
+            }}
+          />
+        </Col>
+      </Row>
+    </Container>
+  );
+};
 
 export default HomePage;
-
-
-
-   
