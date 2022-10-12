eaafa124dbcc640c3aa97fa0f708d9945d571a05
Abis <63680552+SirL0gic@users.noreply.github.com>
2022-10-12 09:50:28 +0400

    Update server.js

--- server/server.js
+++ server/server.js
@@ -2,8 +2,11 @@ const express = require("express");
 const { restart } = require("nodemon");
 const app = express();
 
+/get request
 app.get("/api", (req, res) => {
     res.json({"users":["user1","user2","user3"]})
   });
 
+
+//server listen
 app.listen(5000, () => {console.log("Server started on 5000")})
