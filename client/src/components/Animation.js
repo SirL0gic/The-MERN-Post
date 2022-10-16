33cf2c0850feb330e127af6ed03318596cdfeb58
Abis <abisali@hotmail.com>
2022-10-16 12:08:23 +0400

    update

--- client/src/components/Animation.js
+++ client/src/components/Animation.js
@@ -4,7 +4,7 @@ let AnimationNews = () => {
     return(
         <div className="hwrap">
         <div className="hmove">
-        <div className="hitem">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</div>
+        <div className="hitem"> Queen Elizabeth II, the UK's longest-serving monarch, has died at Balmoral aged 96.</div>
         <div className="hitem">Aliquam consequat varius consequat.</div>
         <div className="hitem">Fusce dapibus turpis vel nisi malesuada sollicitudin.</div>
         <div className="hitem">Pellentesque auctor molestie orci ut blandit.</div>
