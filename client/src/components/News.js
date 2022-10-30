1d17f78b3b7a6d49ed3b337f0c2e6cdd9af60bf0
Abis <abisali@hotmail.com>
2022-10-30 12:34:07 +0400

    props

--- client/src/components/News.js
+++ client/src/components/News.js
@@ -2,24 +2,17 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import '../css/newscard.css';
 
-let NewsCard = () => {
+let NewsCard = (props) => {
   return (
     <div class="card">
       <img
-        src="https://codingyaar.com/wp-content/uploads/bootstrap-4-card-image-left-demo-image.jpg"
+        src={props.image}
         class="card-img-top"
       />
       <div class="card-body">
-        <h5 class="card-title">Card Title</h5>
+        <h5 class="card-title">{props.title}</h5>
         <p class="card-text">
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
-       
-        </p>
-        <p class="card-text">
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
-       
+        {props.mini}
         </p>
         <a href="#" class="btn btn-dark ">
           Go somewhere
