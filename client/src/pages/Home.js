9c2fd898f65ae96d7eef3cd6248d716a3ed0bc7e
Abis <abisali@hotmail.com>
2022-10-30 12:39:18 +0400

    Update Home.js

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -61,7 +61,7 @@ let HomePage = () => {
             <NewsCard title="Elon Musk planned Twitter layoffs are imminent" author="Gerrit De Vynck" mini="Elon Musks plans to lay off large number of Twitter employees are getting underway, a course of action that could prompt fundamental changes in the way more than 200 million daily users experience the site." image="https://www.washingtonpost.com/wp-apps/imrs.php?src=https://arc-anglerfish-washpost-prod-washpost.s3.amazonaws.com/public/WKECRV6SQ6EZHD4KAUXNIPE3VM.JPG&w=916" />
             <br></br>
             <br></br>
-            <NewsCard />
+            <NewsCard title="Russia pauses grain deal after Ukraine strikes warships in Crimea" author="" mini="" image="" />
             <br></br>
             <br></br>
             <NewsCard />
