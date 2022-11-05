c37552ab9da814b25fa350b192cfc48a1820d91b
Abis <abisali@hotmail.com>
2022-11-05 13:31:16 +0400

    Update Read.js

---
+++ client/src/pages/Read.js
@@ -0,0 +1,22 @@
+//Importing-Hooks
+import React, { useState, useEffect } from "react";
+
+// Bootstrap-Bundle-JS
+import "bootstrap/dist/js/bootstrap.bundle.min";
+
+// Bootstrap-npm-lib
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+
+//Bootstrap-CSS
+import "bootstrap/dist/css/bootstrap.min.css";
+
+let ReadPage = () => {
+    return (
+
+
+    )
+};
+
+export default Readpage
