38b918c6b2418e1bcfaa9b43993f77e594e4d7f7
Abis <abisali@hotmail.com>
2022-11-06 12:48:51 +0400

    struct

--- client/src/pages/Read.js
+++ client/src/pages/Read.js
@@ -15,32 +15,49 @@ import "bootstrap/dist/css/bootstrap.min.css";
 //Importing-Components..
 import NavBar from "../components/Nav";
 import FooterComp from "../components/Footer";
+import Header from "../components/Head";
+import DateTime from "../components/Date";
 
 //Custom-CSS
 import "../css/read.css";
 
 let ReadPage = (props) => {
-    return (
-        <Container fluid className="main-container">
-           <Row className="navigation-row">
-            <Col lg={12} className="navigation-col-one">
-                <NavBar />
-            </Col>
-            {props.title}
-           </Row>
+  return (
+    <Container fluid className="main-container">
+      <Row className="navigation-row">
+        <Col lg={12} className="navigation-col-one">
+          <NavBar />
+        </Col>
+        {props.title}
+      </Row>
 
-           <Row className="heading-row"></Row>
-           <Row className="new-article-row"></Row>
+      <Row className="heading-row">
+        <Col lg={12} className="navigation-col-one">
+          <div className="text-center">
+            <h1 className="text-center">{<Header />}</h1>
+            <p className="date-display">{<DateTime />}</p>
+          </div>
+        </Col>
+      </Row>
 
-           <Row className="footer-row">
-           <Col lg={12} className="footer-col-one">
-           <FooterComp />
-            </Col>
-            </Row>
-        </Container>
+      <Row className="new-article-row">
+        <Col lg={12} className="news-article-col-one">
+            <h1>Title</h1>
+            <h2>Sub Title</h2>
+            <p>Date</p>
+            <p>Author</p>
+            <p>image</p>
+            <p>body</p>
+        </Col>
+      </Row>
 
-
-    )
-}
+      <Row className="footer-row">
+        <Col lg={12} className="footer-col-one">
+          <FooterComp />
+        </Col>
+      </Row>
+    </Container>
+  );
+};
 
 export default ReadPage;
