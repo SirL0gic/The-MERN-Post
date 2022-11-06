4638abb16dcaae3fb7694f37fef014f327549bc2
Abis <abisali@hotmail.com>
2022-11-06 13:05:41 +0400

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -27,9 +27,17 @@ function App() {
     <Routes>
       <Route path="/" element={ <HomePage/> } />
       <Route path="sports" element={ <SportsPage/> } />
-      <Route path="read-elon" element={ <Readpage title="Elon Musk’s planned Twitter layoffs are imminent" 
+      <Route path="read-elon" 
+      element={ <Readpage 
+      title="Elon Musk’s planned Twitter layoffs are imminent" 
       subtitle="Tesla engineers were on-site Friday to evaluate the Twitter staff’s code, workers said, as anxiety built around Musk’s silence" 
-      date="October 29, 2022"/> } />
+      date="October 29, 2022"
+      author="Gerrit De Vynck"
+      image="https://www.washingtonpost.com/wp-apps/imrs.php?src=https://arc-anglerfish-washpost-prod-washpost.s3.amazonaws.com/public/WKECRV6SQ6EZHD4KAUXNIPE3VM.JPG&w=916"
+      body=""
+      />
+      
+      } />
     </Routes>
     </div>
 
