9aaf048c7f026e14ef3ddb4a82fe560b46de4524
Abis <63680552+SirL0gic@users.noreply.github.com>
2022-11-07 12:17:11 +0400

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -14,7 +14,7 @@ function App() {
     <div className="App">
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="sports" element={<SportsPage />} />
+        {/* <Route path="sports" element={<SportsPage />} /> */}
         <Route
           path="read-elon"
           element={
