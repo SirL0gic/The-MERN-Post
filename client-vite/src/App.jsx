338419890240b214339377d84ba04c401bc38819
Abis <63680552+SirL0gic@users.noreply.github.com>
2023-09-01 11:11:17 +0400

    update

--- client-vite/src/App.jsx
+++ client-vite/src/App.jsx
@@ -1,16 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-   <h1>hello</h1>
+      <Routes></Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
