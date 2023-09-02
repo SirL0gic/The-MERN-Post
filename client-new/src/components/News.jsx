221c475f71de7c73cceb65ecee5ae5bd246f9c70
Abis <abisali@hotmail.com>
2023-09-03 00:46:48 +0400

    update

---
+++ client-new/src/components/News.jsx
@@ -0,0 +1,28 @@
+import '../css/newscard.css';
+
+
+let NewsCard = (props) => {
+  return (
+    <div class="card">
+      <img
+        src={props.image}
+        class="card-img-top"
+        id="new-imgg"
+        height={props.height}
+      />
+      <div class="card-body">
+        <h4 class="card-title">{props.title}</h4>
+        <p class="card-text">By: {props.author}</p>
+        <br></br>
+        <p class="card-text">
+        {props.mini}
+        </p>
+        <a href={props.link} class="btn btn-dark" id="linker">
+          Read More
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default NewsCard;
--- client-new/src/pages/Home.jsx
+++ client-new/src/pages/Home.jsx
@@ -8,6 +8,7 @@ import NavBar from "../components/Nav";
 import Header from "../components/Head";
 import DateTime from "../components/Date";
 import AnimationNews from "../components/Animation";
+import NewsCard from "../components/News";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import "../App.css";
@@ -39,6 +40,45 @@ let HomePage = () => {
             </div>
           </Col>
         </Row>
+
+        
+      <Row>
+        <Col lg={8} className="news-container">
+          <div className="news-area">
+            <h1>News Feed</h1>
+            <br></br>
+            <NewsCard
+              title="Elon Musk planned Twitter layoffs are imminent"
+              link="/read-elon"
+              height="440px"
+              author="Gerrit De Vynck"
+              mini="Elon Musks plans to lay off large number of Twitter employees are getting underway, a course of action that could prompt fundamental changes in the way more than 200 million daily users experience the site."
+              image="https://www.washingtonpost.com/wp-apps/imrs.php?src=https://arc-anglerfish-washpost-prod-washpost.s3.amazonaws.com/public/WKECRV6SQ6EZHD4KAUXNIPE3VM.JPG&w=916"
+            />
+            <br></br>
+            <br></br>
+            <NewsCard
+              title="Russia pauses grain deal after Ukraine strikes warships in Crimea"
+              link="/read-russia"
+              height="460px"
+              author="Mary Ilyushina"
+              mini="Russia suspended its participation in the U.N.-brokered deal that allowed Ukraine to export its grain and other agricultural products from Black Sea ports after claiming that Kyiv used the corridor to attack Kremlin ships, reigniting concerns about global food insecurity."
+              image="https://www.washingtonpost.com/wp-apps/imrs.php?src=https://arc-anglerfish-washpost-prod-washpost.s3.amazonaws.com/public/KNNJEFH4DFY3QFHPLNPWL7LFAE.jpg&w=916"
+            />
+            <br></br>
+            <br></br>
+            <NewsCard
+              title="Inside Google new Halloween-themed playable Doodle"
+              link="/read-google"
+              height="480px"
+              author="Shannon Liao"
+              mini="When people open Google.com, the tech company Doodle team has a few seconds to hook a visitor into engaging with their games, illustrations or historical facts. This Halloween, Google is inviting people to play an online multiplayer game akin to arcade and classic phone game “Snake.”"
+              image="https://www.washingtonpost.com/wp-apps/imrs.php?src=https://arc-anglerfish-washpost-prod-washpost.s3.amazonaws.com/public/EKTPQI3ZQNEC7K5UB6DKDMDT5Y.jpg&w=1440"
+            />
+          </div>
+        </Col>
+        </Row>
+
       </Container>
     </main>
   );
