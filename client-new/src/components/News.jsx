cc701b1ebe293943c755359f9a2aa8d0a72ad7a6
Abis <63680552+SirL0gic@users.noreply.github.com>
2023-09-07 15:59:49 +0400

    Update News.jsx

--- client-new/src/components/News.jsx
+++ client-new/src/components/News.jsx
@@ -5,12 +5,10 @@ let NewsCard = (props) => {
         src={props.image}
         className="card-img-top"
         id="new-imgg"
-        // height={props.height}
       />
       <div className="card-body">
         <h4 className="card-title">{props.title}</h4>
         <p className="card-text">By: {props.author}</p>
-        <br></br>
         <p className="card-text">{props.mini}</p>
         <a href={props.link} className="btn btn-dark" id="linker">
           Read More
