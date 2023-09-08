2574d58878e38a5779068ac208a37b9d0cc25c46
Abis <63680552+SirL0gic@users.noreply.github.com>
2023-09-08 10:51:16 +0400

    update

--- server/server.js
+++ server/server.js
@@ -1,13 +1,19 @@
 //Imports
 const express = require("express");
 const NewsAPI = require("newsapi");
-const newsapi = new NewsAPI("");
+const dotenv = require("dotenv");
+
 
 //Backend Config
 const app = express();
 const host = "127.0.0.1";
 const public_host = "0.0.0.0";
 const port = 4000;
+
+//Env variables
+dotenv.config();
+const newsapi = new NewsAPI(process.env.NEWSAPIKEY);
+
 
 //For cross orgin requests and Enable CORS for all routes.
 const cors = require("cors");
@@ -18,27 +24,21 @@ app.get("/api/test", (req, res) => {
 });
 
 app.get("/api/news", (req, res) => {
-  // To query /v2/everything
   // You must include at least one q, source, or domain
+
   newsapi.v2
     .everything({
       q: "bitcoin",
       sources: "bbc-news,the-verge",
       domains: "bbc.co.uk, techcrunch.com",
-      from: "2017-12-01",
-      to: "2017-12-12",
+      from: "2023-08-10",
+      to: "2023-09-08",
       language: "en",
       sortBy: "relevancy",
-      page: 2,
+      page: 1,
     })
     .then((response) => {
       console.log(response);
-      /*
-      {
-        status: "ok",
-        articles: [...]
-      }
-    */
     });
 });
 
