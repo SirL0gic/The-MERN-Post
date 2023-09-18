8aa5e50b578c68c29aefb04f752a355e772e89c0
Abis <63680552+SirL0gic@users.noreply.github.com>
2023-09-18 11:58:50 +0400

    update

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ const dotenv = require("dotenv");
 const axios = require("axios");
 const { MongoClient, ServerApiVersion } = require("mongodb");
 const rateLimit = require("express-rate-limit");
-const cors = require("cors");
+// const cors = require("cors");
 
 dotenv.config();
 
@@ -14,30 +14,58 @@ const PORT = 4000;
 const MONGODB_URI = process.env.MONGODB_URI;
 const WEATHER_API_KEY = process.env.WEATHERAPI;
 
-const ALLOWED_ORIGINS = [
-  "https://www.thereactpost.xyz",
-  "https://thereactpost.xyz",
-];
 
 // Initialize Express app
 const app = express();
 
+// app.use((req, res, next) => {
+//   console.log("Received headers:", req.headers);
+//   next();
+// });
+
 // CORS Configuration
-app.use(
-  cors({
-    origin: (origin, callback) => {
-      if (ALLOWED_ORIGINS.includes(origin) || !origin) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
-  })
-);
+app.use((req, res, next) => {
+  const allowedOrigins = ["https://www.thereactpost.xyz", "https://thereactpost.xyz"];
+  const origin = req.headers.origin;
+
+  if (allowedOrigins.includes(origin)) {
+      res.header("Access-Control-Allow-Origin", origin);
+      res.header("Access-Control-Allow-Methods", "GET,POST");
+      res.header("Access-Control-Allow-Headers", "Content-Type");
+      next();
+  } else {
+    return res.status(403).send(`Unauthorized origin: ${origin}`);
+
+  }
+});
+
+
+// app.use(cors());
+// app.use(
+//   cors({
+//     origin: (origin, callback) => {
+//       if (origin === "https://www.thereactpost.xyz" || origin === "https://thereactpost.xyz") {
+//         callback(null, true);
+//       } else {
+//         callback(new Error("Not allowed by CORS"));
+//       }
+//     },
+//   })
+// );
+
+//Custom error handler for CORS errors
+// app.use((err, req, res, next) => {
+//   if (err.message === "Not allowed by CORS") {
+//     res.status(403).send("CORS Error: Not allowed by CORS");
+//   } else {
+//     next(err);
+//   }
+// });
 
 app.use(compression());
 app.use(express.json());
 
+app.set('trust proxy', true); // to trust x-forward
 
 // Rate Limiting Middleware
 app.use(
@@ -54,7 +82,9 @@ app.post("/api/weather", async (req, res) => {
   if (!ip) return res.status(400).json({ error: "IP address is required" });
 
   try {
-    const response = await axios.get(`http://api.weatherapi.com/v1/current.json?key=${WEATHER_API_KEY}&q=${ip}`);
+    const response = await axios.get(
+      `http://api.weatherapi.com/v1/current.json?key=${WEATHER_API_KEY}&q=${ip}`
+    );
     const data = response.data;
 
     return res.json({
@@ -70,7 +100,9 @@ app.post("/api/weather", async (req, res) => {
   }
 });
 
-app.get("/api/test", (req, res) => res.send(`Server is running on ${HOST}:${PORT}`));
+app.get("/api/test", (req, res) =>
+  res.send(`Server is running on ${HOST}:${PORT}`)
+);
 
 app.get("/api/mongo-test", async (req, res) => {
   const client = new MongoClient(MONGODB_URI, {
@@ -94,19 +126,27 @@ app.get("/api/mongo-test", async (req, res) => {
 app.get("/api/top-headlines", async (req, res) => {
   const DATABASE_NAME = "News";
   const COLLECTION_NAME = "top-news";
+
   const client = new MongoClient(MONGODB_URI, {
-    useUnifiedTopology: true,
-    useNewUrlParser: true,
-    serverApi: ServerApiVersion.v1,
+    serverApi: {
+      version: ServerApiVersion.v1,
+      strict: true,
+      deprecationErrors: true,
+    },
   });
 
   try {
     await client.connect();
-    const articles = await client.db(DATABASE_NAME).collection(COLLECTION_NAME).find().limit(10).toArray();
-    return res.json(articles);
+
+    const db = client.db(DATABASE_NAME);
+    const collection = db.collection(COLLECTION_NAME);
+
+    const articles = await collection.find().toArray();
+
+    res.status(200).send(articles.slice(0, 10));
+    console.log("db working");
   } catch (error) {
-    console.error("Error fetching top headlines:", error);
-    return res.status(500).send("Error fetching top headlines");
+    res.status(500).send("Error occurred: " + error.message);
   } finally {
     await client.close();
   }
